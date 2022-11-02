69a05c4b98261c5a9afc5137a78994ff251d001d
Trescovius <emes.davidt@gmail.com>
2022-11-02 15:59:46 +0000

    creating the necessary files

---
+++ Results.R
@@ -0,0 +1,2 @@
+
+#'This file contains the code needed to generate results using the AHHME-B model
---
+++ ShinyApp.R
@@ -0,0 +1,3 @@
+
+#'This file contains the script for the interactive online application (ShinyApp)
+#'which generates results using the AHHME-B model
